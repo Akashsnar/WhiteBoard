f27aa2d45c6e0809e7cf41388e65be093d285e58
Akashsnar <aksn0204@gmail.com>
2026-04-07 13:31:36 +0530

    added: signup button

--- frontend/src/pages/CanvasRoom.jsx
+++ frontend/src/pages/CanvasRoom.jsx
@@ -2,7 +2,7 @@ import { useRef, useEffect, useState } from "react";
 import { io } from "socket.io-client";
 import { useParams } from 'react-router-dom';
 
-const socket = io(import.meta.env.BACKEND_URL);
+const socket = io(import.meta.env.VITE_BACKEND_URL);
 
 const CanvasBoard = () => {
   const { id: canvasId } = useParams();
